a2da54fb7c5e4747531773e578537a0c53966e29
MaxKanter24 <max.kanter24@bcp.org>
2023-05-05 08:32:23 -0700

    added pygame window

--- final.py
+++ final.py
@@ -3,6 +3,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import pygame
 
 city = input("City:")
 state = input("State:")
@@ -20,3 +21,30 @@ low_temp = bs.find("span", attrs={'class':'lo'}).text
 
 print("HIGH:", high_temp)
 print("LOW:", low_temp)
+
+#initialises pygame
+pygame.init()
+
+screen_width, screen_height = 680, 480
+font = pygame.font.SysFont('TimesNewRoman', 30)
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Weather")
+# Loop to keep the window open
+while True:
+    # Check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()  
+            
+    screen.fill((255, 255, 255))
+
+    # Render weather information as text and display in the center of the screen
+    high_text = font.render(f"HIGH: {high_temp}", True, (0, 0, 0))
+    low_text = font.render(f"LOW: {low_temp}", True, (0, 0, 0))
+    screen.blit(high_text, ((screen_width - high_text.get_width()) // 2, (screen_height - high_text.get_height()) // 2 - 50))
+    screen.blit(low_text, ((screen_width - low_text.get_width()) // 2, (screen_height - low_text.get_height()) // 2 + 50))
+
+    # Update the display
+    pygame.display.update()
