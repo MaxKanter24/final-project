313ff3a866f4e2da18a1a5fbe394c00b4824b4b1
MaxKanter24 <max.kanter24@bcp.org>
2023-05-18 23:56:09 -0700

    add images for weather

--- finalproject.py/final.py
+++ finalproject.py/final.py
@@ -1,35 +1,48 @@
 # file created by Max Kanter
 # followed Andrew Negrut 
+"Resources:"
+"https://www.youtube.com/watch?v=cta1yCb3vA8&t=20s"
+"https://automatetheboringstuff.com/"
+"https://www.youtube.com/watch?v=4XQHZIk9gxc"
+"in class code"
 
 from bs4 import BeautifulSoup
 import requests
-import pygame
+import pygame 
+import os
 
+game_folder = os.path.dirname(__file__)
+print(game_folder)
+
+# Enter location:
+# If city is two words, add a dash: 'san-jose'
+# Short abbreviation of state: 'ca'
 city = input("City:")
 state = input("State:")
 
-# if city is two words, add dash: san-jose
-# short abreviation of state: ca
 url = f"https://www.wunderground.com/weather/us/{state}/{city}"
-
 request = requests.get(url)
-
-bs = BeautifulSoup(request.content, features= 'lxml')
-
+bs = BeautifulSoup(request.content, features='lxml')
+# Find specific high and low temperature
 high_temp = bs.find("span", attrs={'class':'hi'}).text
 low_temp = bs.find("span", attrs={'class':'lo'}).text
-
 print("HIGH:", high_temp)
 print("LOW:", low_temp)
 
-#initialises pygame
+# Initialize pygame
 pygame.init()
 
-screen_width, screen_height = 680, 480
+# Settings for pygame window
+WIDTH = 680
+HEIGHT = 480
+FPS = 30
 font = pygame.font.SysFont('TimesNewRoman', 30)
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Weather")
+
+shorts_image = pygame.image.load(os.path.join(game_folder, 'shorts.jpg')).convert()
+
 # Loop to keep the window open
 while True:
     # Check for events
@@ -37,14 +50,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()  
-            
+         
     screen.fill((255, 255, 255))
 
     # Render weather information as text and display in the center of the screen
     high_text = font.render(f"HIGH: {high_temp}", True, (0, 0, 0))
     low_text = font.render(f"LOW: {low_temp}", True, (0, 0, 0))
-    screen.blit(high_text, ((screen_width - high_text.get_width()) // 2, (screen_height - high_text.get_height()) // 2 - 50))
-    screen.blit(low_text, ((screen_width - low_text.get_width()) // 2, (screen_height - low_text.get_height()) // 2 + 50))
+    screen.blit(high_text, ((WIDTH - high_text.get_width()) // 2, (HEIGHT - high_text.get_height()) // 5 - 15))
+    screen.blit(low_text, ((WIDTH - low_text.get_width()) // 2, (HEIGHT - low_text.get_height()) // 4 + 15))
+    screen.blit(shorts_image, (0, 0))
 
     # Update the display
     pygame.display.update()
